aa8d7250c04e8483a87ed378ab77203f655000f8
summit4you <summit_mail@qq.com>
2012-02-08 12:27:36 +0800

    finish login

--- src/login/panel.js
+++ src/login/panel.js
@@ -8,6 +8,7 @@ Ext.define('Af.login.panel', {
 		width: '320',
 		layout:'vbox',
 		id:'loginpanel',
+		scroll: true,
 		items:[
 			{
 				xtype: 'navigationbar',
@@ -41,6 +42,98 @@ Ext.define('Af.login.panel', {
 				xtype: 'panel',
 				cls: 'login-logo-panel',
 				html:'<img src="resources/themes/images/default/logo_icon.png" height="45px"/>'
+			},
+			{
+				xtype: 'formpanel',
+				cls: 'login-formpanel',
+				items: [
+					{
+						xtype: 'panel',
+						cls: 'login-number-panel',
+						items: [
+						{
+							xtype: 'numberfield',
+							name: 'phoneNumber',
+							placeHolder: '15012378921',
+							useClearIcon: true,
+						}],
+						
+					},
+					{
+						xtype: 'panel',
+						cls: 'login-psw-panel',
+						items: [
+						{
+							xtype: 'passwordfield',
+							name: 'psw',
+							placeHolder: '密码',
+							useClearIcon: true,
+						}],
+					},
+					{
+						xtype: 'panel',
+						cls: 'login-verify-panel',
+						layout:'hbox',
+						items:[
+							{
+								xtype: 'passwordfield',
+								name: '',
+								placeHolder: '验证码',
+								useClearIcon: true,
+								flex:1,
+							},
+							{
+								xtype: 'button',
+								cls: 'login-vbutton',
+								pressedCls: 'login-vbutton-pressed',
+								baseCls: 'x-button-af',
+								html: '<b>获取验证码</b>',
+								flex:1
+							},
+						]
+					},
+					{
+						xtype: 'button',
+						cls: 'login-cbutton',
+						pressedCls: 'login-cbutton-pressed',
+						baseCls: 'x-button-af',
+						height: '30px',
+						html: '<b>登&nbsp;&nbsp;录</b>',
+					},
+					{
+						xtype: 'button',
+						cls: 'login-cbutton',
+						pressedCls: 'login-cbutton-pressed',
+						baseCls: 'x-button-af',
+						height: '30px',
+						html: '<img src="resources/themes/images/default/sina_logo.png" height="20px" style="vertical-align:text-bottom;"/><b>新浪微博登陆</b>',
+					},
+					{
+						xtype: 'button',
+						cls: 'login-cbutton',
+						pressedCls: 'login-cbutton-pressed',
+						baseCls: 'x-button-af',
+						height: '30px',
+						html: '<b>马上注册一个</b>',
+					},
+					
+				],
+				flex:1
+			},
+			{
+				xtype: 'panel',
+				cls: 'login-rbutton-panel',
+				items:[
+					{
+						xtype: 'button',
+						cls: 'login-rbutton',
+						docked: 'right',
+						pressedCls: 'login-rbutton-pressed',
+						baseCls: 'x-button-af',
+						height: '30px',
+						html: '<img src="resources/themes/images/default/q_icon2.png" height="30px"/>',
+					}
+				]
 			},
 		
 		]
