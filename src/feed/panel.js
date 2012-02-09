659bd5de73642c42671084457990f76b3ac9260f
heshiyou <ernest.he2009@gmail.com>
2012-02-09 11:15:48 +0800

    simple list of article

--- src/feed/panel.js
+++ src/feed/panel.js
@@ -1,3 +1,24 @@
+Ext.regModel('Article', {
+   fields: ['usrImg', 'username', 'score', 'class', 'address', 'review', 'fans', 'distance', 'hot']
+});
+
+var store = new Ext.data.JsonStore({
+   model: 'Article',
+   //sorters: 'username',
+
+   getGroupString: function(record) {
+       return record.get('username')[0];
+   },
+
+   data: [
+       {usrImg: 'resources/themes/images/default/friends/1.png',   username: '王品台塑牛排', score:'90', title:'西式餐馆', address:'天河粤垦路188号', review:'0', fans:'0', distance:'0.8', hot:'270', isset:'1'},
+       {usrImg: 'resources/themes/images/default/friends/2.png',   username: '日本鱼子饭', score:'120', title:'日式餐馆', address:'天河粤垦路188号', review:'2', fans:'32', distance:'0.9', hot:'159', isset:'1'},
+       {usrImg: 'resources/themes/images/default/friends/3.png',   username: 'AngleSimple', score:'217', title:'艺术蛋糕', address:'天河粤垦路188号', review:'2', fans:'32', distance:'0.5', hot:'346', isset:'0'},
+       {usrImg: 'resources/themes/images/default/friends/4.png',   username: '日本小丸子', score:'321', title:'日式餐馆', address:'天河粤垦路188号', review:'2', fans:'32', distance:'0.9', hot:'215', isset:'0'},
+       {usrImg: 'resources/themes/images/default/friends/5.png',   username: '王品台塑牛排', score:'90', title:'西式餐馆', address:'天河粤垦路188号', review:'0', fans:'0', distance:'0.8', hot:'270', isset:'1'}
+   ]
+});
+
 Ext.define('Af.feed.panel', {
 	
 	extend: 'Ext.Panel',
@@ -8,7 +29,9 @@ Ext.define('Af.feed.panel', {
 		width: '640',
 		layout: 'vbox',
 		id: 'feedpanel',
-		items: [{
+		items: [
+		// top bar
+		{
 			xtype: 'navigationbar',
 			title: '<b>动&nbsp;&nbsp;态</b>',
 			ui: 'af',
@@ -50,7 +73,19 @@ Ext.define('Af.feed.panel', {
 					flex:0.14
 				},
 			]
-		}
+		},
+		// switch bar
+		// horizontal boxs
+		 
+		// articles list
+		{
+			xtype: 'list',
+			cls: 'articles-list',
+			selectedCls: 'articles-list-selectd',
+			itemTpl: '<div class="user" style="float:left;"><img src={usrImg} width="60px" style="float:left;"/><span style="float:left;margin-top:65px; margin-left:-60px; ">{username}</span></div> <div class="article"><div class="article-title">{title}</div><div class="hot">{article-hot}</div><div class="article-content">{address}</div><div class="article-time">{distance}</div><div class="article-actions"></div></div>',
+			store: store,
+			flex: 1,
+		}		
 		]
 	},
 	initComponent: function() {
