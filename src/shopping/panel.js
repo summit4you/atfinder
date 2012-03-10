bf9cf91f7b14e7d81f17d99377335ba717bd6d5f
heshiyou <ernest.he2009@gmail.com>
2012-03-11 04:46:25 +0800

    fix toolbar select style

--- src/shopping/panel.js
+++ src/shopping/panel.js
@@ -16,7 +16,7 @@ var store = Ext.create('Ext.data.Store',{
    }},
    
    data: [
-       {usrImg: 'resources/themes/images/default/friends/1.png',   username: '王品台塑牛排', score:'90', class:'西式餐馆', address:'天河粤垦路188号天河粤垦路188号天河粤垦路188号', review:'0', fans:'0', distance:'0.8', hot:'270', isset:'1', validity:'2012年3月1日前', origin_price:'135', price:'235', cutoff:'90'},
+       {usrImg: 'resources/themes/images/default/friends/1.png',   username: '王品台塑牛排', score:'90', class:'西式餐馆', address:'天河粤垦路188号', review:'0', fans:'0', distance:'0.8', hot:'270', isset:'1', validity:'2012年3月1日前', origin_price:'135', price:'235', cutoff:'90'},
        {usrImg: 'resources/themes/images/default/friends/2.png',   username: '日本鱼子饭', score:'120', class:'日式餐馆', address:'天河粤垦路188号', review:'2', fans:'32', distance:'0.9', hot:'159', isset:'1',validity:'2012年3月1日前', origin_price:'135', price:'235', cutoff:'90'},
        {usrImg: 'resources/themes/images/default/friends/3.png',   username: 'AngleSimple', score:'217', class:'艺术蛋糕', address:'天河粤垦路188号', review:'2', fans:'32', distance:'0.5', hot:'346', isset:'0', validity:'2012年3月1日前', origin_price:'135', price:'235', cutoff:'90'},
        {usrImg: 'resources/themes/images/default/friends/4.png',   username: '日本小丸子', score:'321', class:'日式餐馆', address:'天河粤垦路188号', review:'2', fans:'32', distance:'0.9', hot:'215', isset:'0', validity:'2012年3月1日前', origin_price:'135', price:'235', cutoff:'90'},
@@ -48,9 +48,11 @@ Ext.define('Af.shopping.panel', {
 		width: '640',
 		layout:'vbox',
 		id:'shoppingpanel',
+		/*
 		scrollable: {
-					direction: 'vertical'
-		},
+							direction: 'vertical'
+				},*/
+		
 		items:[
 			{
 				xtype: 'titlebar',
@@ -275,122 +277,7 @@ Ext.define('Af.shopping.panel', {
 					}
 				]
 			},
-			// switch bar
-		/*{
-	        xtype: 'toolbar',
-            //ui: 'af',
-            layout: {
-            	type: 'vbox',
-            	pack: 'center',
-            	align: 'stretch',
-            },
-            height:22,
-            items: [
-                    {
-						xtype:'button',
-						cls: 'shopping-cbutton',
-						baseCls: 'x-button-af',
-                        html: '商&nbsp;&nbsp;家',
-						flex:1,
-						handler: moveSlide
-                    },
-                    {
-						xtype:'button',
-						cls: 'shopping-cbutton-red',
-						baseCls: 'x-button-af',
-                        html: '推荐商品',
-						flex:1,
-						handler: moveSlide
-                    },
-                    {
-						xtype:'button',
-						cls: 'shopping-cbutton',
-						baseCls: 'x-button-af',
-                        html: '优惠券',
-						flex:1,
-						handler: moveSlide
-                    }
-                ]
-		}, 
-		// switch status bar
-		{
-			xtype: 'panel',
-			cls: 'shopping-switch-status',
-			
-			 layout: 'hbox',
-			
-			style: 'background-color: #188fdd;',
-			items:[
-				{
-					xtype:'spacer',
-					flex:1
-				},
-				{
-					id:'a_1',
-					xtype: 'panel',
-					flex: 1,
-					style: 'background-color: red;',
-				
-				},
-				{
-					xtype:'spacer',
-					flex:1
-				}
-			],
-			
-			flex: 0.005
-		},
-		// content
-		{
-			xtype: 'panel',
-			flex: 1,
-			layout: 'card',
-			items: [
-/*			// shop list
-			{
-				xtype: 'list',
-				cls: 'shopping-list',
-				selectedCls:  'shopping-list-selected',
-				itemTpl: new Ext.XTemplate('<img src={usrImg} width="75px" style="float:left;"/>',
-				'<div style="float:left;"><div class="row" style="height:16px;line-height:16px;"><span class="username">{username}</span><img src="resources/themes/images/default/friends_score.png" width="13px" style="margin-left:5px;"/><span class="score">{score}</span></div><div class="row" style="height:16px;line-height:16px;"><span class="class">{class}</span></div><div class="row"  style="height:16px;line-height:16px;"><span class="address">地址:{address}</span></div><div class="row" style="height:12px;line-height:12px;"><span class="review">评价<tpl if="review &gt; 0"><a href="#">({review})</a></tpl>   粉丝<tpl if="fans &gt; 0"><a href="#">({fans})</a></tpl>   距离：{distance}km</span></div></div><div class="hot-panel">',
-				'<span class="',
-				' <tpl if="hot &lt; 270">hot-color-blue</tpl> <tpl if="hot &gt;= 270">hot-color-red</tpl>',
-				' ">{hot}</span><div class="hot-white-font ',
-				'<tpl if="hot &lt; 270">hot-bkcolor-blue</tpl> <tpl else">hot-bkcolor-red</tpl>">℃',
-				'</div></div>'),
-				store: store,
-			},
-			// goods list
-			{
-				xtype: 'list',
-				cls: 'shopping-list',
-				selectedCls:  'shopping-list-selected',
-				itemTpl: new Ext.XTemplate('<img src={usrImg} width="75px" style="float:left;"/>',
-				'<div style="float:left;"><div class="row" style="height:16px;line-height:16px;"><span class="username">{username}</span><img src="resources/themes/images/default/friends_score.png" width="13px" style="margin-left:5px;"/><span class="score">{score}</span></div><div class="row" style="height:16px;line-height:16px;"><span class="class">{class}</span></div><div class="row"  style="height:16px;line-height:16px;"><span class="address">地址:{address}</span></div><div class="row" style="height:12px;line-height:12px;"><span class="review">评价<tpl if="review &gt; 0"><a href="#">({review})</a></tpl>   粉丝<tpl if="fans &gt; 0"><a href="#">({fans})</a></tpl>   距离：{distance}km</span></div></div><div class="hot-panel">',
-				'<span class="',
-				' <tpl if="hot &lt; 270">hot-color-blue</tpl> <tpl if="hot &gt;= 270">hot-color-red</tpl>',
-				' ">{hot}</span><div class="hot-white-font ',
-				'<tpl if="hot &lt; 270">hot-bkcolor-blue</tpl> <tpl else">hot-bkcolor-red</tpl>">℃',
-				'</div></div>'),
-				store: store,
-			},
-*/			// coupon list
-			/*{
-				xtype: 'list',
-				cls: 'coupon-list',
-				selectedCls:  'coupon-list-selected',
-				itemTpl: new Ext.XTemplate(
-				'<img src={usrImg} width="75px" style="float:left;"/>',
-				'<div style="float:left;"><div class="row" style="height:16px;line-height:16px;"><span class="username">{username}</span><img src="resources/themes/images/default/friends_score.png" width="13px" style="margin-left:5px;"/><span class="score">{score}</span></div><div class="row" style="height:16px;line-height:16px;"><span class="validity">时效:{validity}</span></div><div class="row"  style="height:16px;line-height:16px;"><span class="price">原价：<s>￥{origin_price}</s>&nbsp;&nbsp;&nbsp;现价：￥{price}</span></div><div class="row" style="height:12px;line-height:12px;"><span class="review">评价<tpl if="review &gt; 0"><a href="#">({review})</a></tpl>   粉丝<tpl if="fans &gt; 0"><a href="#">({fans})</a></tpl>   距离：{distance}km</span></div></div><div class="cutoff-panel">',
-				'<div class="cutoff-white-font ',
-				'<tpl if="hot &lt; 270">cutoff-bkcolor-blue</tpl> <tpl else">cutoff-bkcolor-red</tpl>',
-				' ">{cutoff}折</div>',
-				'<img class="cutoff-unfolder" src="resources/themes/images/default/coupon_unfolder.png"/>',
-				'</div></div>'),
-				store: store,
-			}
-			]
-		},*/
+
 			//bottom bar
 			{
 				xtype : 'toolbar',
@@ -398,31 +285,36 @@ Ext.define('Af.shopping.panel', {
 				height: 40,
 				ui: 'af3',
 				layout: {
-					type:'hbox',
-					align:'center'
-				},
+									type:'hbox',
+									align:'center'
+								},
+				
 				items:[
+					
 					{
 						xtype: 'spacer',
 						//width: 25,
 						//flex: 50/640,
 						flex: 0.074
-                	},
+					},
 					{
-						xtype: 'button',
-						cls: 'af-toolbar-cbutton',
-						baseCls: 'x-button-af',
-						html: '<img src="resources/themes/images/default/shopping_foot.png" width="120px"><span style="color:#dcc;margin-left:-115px;">附近0.8km</span>',
-						handler: function(){
-							alert(12);
-						}
+						xtype: 'selectfield',
+						name: 'rank',
+						ui: 'af',
+						width: 120,
+						options: [
+							        {text: '附近800m',  value: 'first'},
+							        {text: '附近1km', value: 'second'},
+							        {text: '附近2km',  value: 'third'}
+							    ],
+						
 					},
 					{
 						xtype: 'spacer',
 						//width: 30,
 						//flex: 61/640,
 						flex: 0.075
-                	},
+					},
 					
 					{
 						xtype: 'button',
@@ -435,40 +327,29 @@ Ext.define('Af.shopping.panel', {
 						//width: 40,
 						//flex: 80/640,
 						flex: 0.075
-                	},
+					},
+					
 					{
 						xtype: 'selectfield',
 						name: 'rank',
+						ui: 'af',
+						width: 120,
 						options: [
 							        {text: '附近800m',  value: 'first'},
 							        {text: '附近1km', value: 'second'},
 							        {text: '附近2km',  value: 'third'}
 							    ],
-						border: '0 0 0 0',
-						width: 120,
-						//cls: 'af-toolbar-cbutton',
-						//baseCls: 'x-button-af',
-						//html: '<img src="resources/themes/images/default/shopping_foot.png" width="120px"></img>',
-						/*
-						handler: function() {
-													var popup = Ext.create('Ext.field.Select', {
-														options: [
-															{text: 'First Option',  value: 'first'},
-															{text: 'Second Option', value: 'second'},
-															{text: 'Third Option',  value: 'third'}
-														]
-													});
-													popup.show();
-												}*/
 						
 					},
+					
 					
 					{
 						xtype: 'spacer',
 						//width: 26,
 						//flex: 52/640,
 						flex: 0.0725
-                	},
+					},
+					
 				]
 			}
 		]
